4d0ee56987e7ebaf6033cef2cf9906b163d0430b
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-09-18 02:31:35 +0700

    Fix language parsing issue

--- website/.vitepress/theme/utils/languages.ts
+++ website/.vitepress/theme/utils/languages.ts
@@ -1,23 +1,40 @@
 // Copyright (c) The Tachiyomi Open Source Project
 // SPDX-License-Identifier: MPL-2.0
 
+// Repos may ship pseudo-languages that aren't valid BCP 47 tags ("none",
+// "other", ...). `Intl.DisplayNames` throws a RangeError on those, which would
+// otherwise blow up sorting/rendering of the whole extension list.
+function displayName(code: string, locale: string) {
+  try {
+    return new Intl.DisplayNames([locale], { type: 'language' }).of(code) ?? code
+  }
+  catch {
+    return code
+  }
+}
+
 export function simpleLangName(code: string) {
   if (code === 'all')
-  return 'All'
-  
-  const namesInEnglish = new Intl.DisplayNames(['en'], { type: 'language' })
-  return capitalize(namesInEnglish.of(code)!, 'en')
+    return 'All'
+
+  return capitalize(displayName(code, 'en'), 'en')
 }
 
 export function langName(code: string) {
   if (code === 'all')
-  return 'All'
-  
-  const namesInEnglish = new Intl.DisplayNames(['en'], { type: 'language' })
-  const namesInNative = new Intl.DisplayNames([code], { type: 'language' })
-  return `${capitalize(namesInEnglish.of(code)!, 'en')} - ${capitalize(namesInNative.of(code)!, code)}`
+    return 'All'
+
+  const inEnglish = capitalize(displayName(code, 'en'), 'en')
+  const inNative = capitalize(displayName(code, code), code)
+
+  return inEnglish === inNative ? inEnglish : `${inEnglish} - ${inNative}`
 }
 
 function capitalize(string: string, locale: string) {
-  return string.charAt(0).toLocaleUpperCase(locale) + string.substring(1)
+  try {
+    return string.charAt(0).toLocaleUpperCase(locale) + string.substring(1)
+  }
+  catch {
+    return string.charAt(0).toUpperCase() + string.substring(1)
+  }
 }
